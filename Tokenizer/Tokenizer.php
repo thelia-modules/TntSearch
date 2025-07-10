9065e841d72bdc85d39665a24a66c9176c7e06f2
Bertrand Tourlonias <btourlonias@openstudio.fr>
2025-07-10 11:49:33 +0200

    Upgrade tntsearch dependency + add boolean search

--- Tokenizer/Tokenizer.php
+++ Tokenizer/Tokenizer.php
@@ -6,12 +6,18 @@ use TeamTNT\TNTSearch\Support\TokenizerInterface;
 
 class Tokenizer extends AbstractTokenizer implements TokenizerInterface
 {
-    static protected $pattern = '/[^\p{L}\p{N}\p{Pc}\p{Pd}@]+/u';
+    static protected $pattern = '/[^\p{L}\p{N}@]+/u';
 
     public function tokenize($text, $stopwords = []): array
     {
-        $text  = mb_strtolower(strip_tags($text));
+        $text = mb_strtolower(strip_tags($text));
+        $text = str_replace(['_', '-', "'", "'"], ' ', $text);
         $split = preg_split($this->getPattern(), $text, -1, PREG_SPLIT_NO_EMPTY);
+
+        $split = array_filter($split, function ($token) {
+            return mb_strlen($token) > 2;
+        });
+
         return array_diff($split, $stopwords);
     }
 }
