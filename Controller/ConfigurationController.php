cf3cfb1a51ae83fdb6bbce3e57897079fb34480a
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-30 09:11:09 +0200

    fix(security): validate CSRF token on configuration update

--- Controller/ConfigurationController.php
+++ Controller/ConfigurationController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Thelia\Controller\Admin\BaseAdminController;
 use Thelia\Core\HttpFoundation\Request;
+use Thelia\Tools\TokenProvider;
 use Thelia\Tools\URL;
 use TntSearch\TntSearch;
 
@@ -13,8 +14,10 @@ use TntSearch\TntSearch;
 class ConfigurationController extends BaseAdminController
 {
     #[Route("", name: "_on_the_fly", methods: ["POST"])]
-    public function configuration(Request $request): RedirectResponse
+    public function configuration(Request $request, TokenProvider $tokenProvider): RedirectResponse
     {
+        $tokenProvider->checkToken((string) $request->request->get('_token'));
+
         $onTheFlyUpdate = (bool)$request->request->get('on-the-fly-update', false);
 
         TntSearch::setConfigValue(TntSearch::ON_THE_FLY_UPDATE, $onTheFlyUpdate);
