1b2eea28091fe45b1c612465a3aee206041bf904
Alexandre Nozière <anoziere@openstudio.fr>
2026-08-19 10:50:56 +0200

    fix: exclude I18n files by their real path so vendor installs are covered

--- TntSearch.php
+++ TntSearch.php
@@ -72,7 +72,7 @@ class TntSearch extends BaseModule
     public static function configureServices(ServicesConfigurator $servicesConfigurator): void
     {
         $servicesConfigurator->load(self::getModuleCode() . '\\', __DIR__)
-            ->exclude([THELIA_MODULE_DIR . ucfirst(self::getModuleCode()) . '/I18n/*'])
+            ->exclude([__DIR__.'/I18n/*'])
             ->autowire()
             ->autoconfigure();
     }
